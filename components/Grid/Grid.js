695c2f2f027b37779e1370ed503092199dd07529
Andy <landywl4@gmail.com>
2022-11-06 18:51:24 -0800

    More test data

--- components/Grid/Grid.js
+++ components/Grid/Grid.js
@@ -95,8 +95,44 @@ export default function Grid() {
         options: ['Test 3-2', 'Not a Test', 'Picklist Stuff', 'Hello World']
       },
       testFieldFour: { value: 'Test 3-3', dataType: 'text' },
-      testFieldFive: { value: 'Test 3-4', dataType: 'text' },
-      testFieldSix: { value: 'Test 3-5', dataType: 'text' }
+      testFieldFive: { value: 'Test 3-4', dataType: 'text' }
+    },
+    {
+      id: 5,
+      testFieldOne: { value: 'Test 5-1', dataType: 'text' },
+      testFieldTwo: {
+        value: 'Test 5-2',
+        dataType: 'picklist',
+        options: ['Test 5-2', 'Not a Test', 'Picklist Stuff', 'Hello World']
+      },
+      testFieldThree: { value: 'Test 5-3', dataType: 'text' },
+      testFieldFive: { value: 'Test 5-5', dataType: 'text' },
+      testFieldSix: { value: 'Test 5-6', dataType: 'text' }
+    },
+    {
+      id: 6,
+      testFieldOne: { value: 'Test 6-1', dataType: 'text' },
+      testFieldTwo: {
+        value: 'Test 6-2',
+        dataType: 'picklist',
+        options: ['Test 6-2', 'Not a Test', 'Picklist Stuff', 'Hello World']
+      },
+      testFieldThree: { value: 'Test 6-3', dataType: 'text' },
+      testFieldFive: { value: 'Test 6-5', dataType: 'text' },
+      testFieldSix: { value: 'Test 6-6', dataType: 'text' }
+    },
+    {
+      id: 7,
+      testFieldOne: { value: 'Test 7-1', dataType: 'text' },
+      testFieldTwo: {
+        value: 'Test 7-2',
+        dataType: 'picklist',
+        options: ['Test 7-2', 'Not a Test', 'Picklist Stuff', 'Hello World']
+      },
+      testFieldThree: { value: 'Test 7-3', dataType: 'text' },
+      testFieldFour: { value: 'Test 7-4', dataType: 'text' },
+      testFieldFive: { value: 'Test 7-5', dataType: 'text' },
+      testFieldSix: { value: 'Test 7-6', dataType: 'text' }
     }
   ]);
 
