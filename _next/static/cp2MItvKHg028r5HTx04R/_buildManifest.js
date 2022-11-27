e73cedec0a52739a2398d02ec6fb29c9ab89897c
Andy <landywl4@gmail.com>
2022-11-27 13:21:03 -0800

    Updates

--- _next/static/cp2MItvKHg028r5HTx04R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/418-f16deb3a89abee90.js","static/css/2b0c8ae08e7f20c6.css","static/chunks/pages/index-f8e661633e6bf932.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
